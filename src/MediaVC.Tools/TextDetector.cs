7f041105e95c6b0314748ccfc48719b3a2102fcd
Mateusz Okrój <mateuszokroj1@hotmail.com>
2021-03-16 23:13:20 +0100

    Created TextDetector class.

---
+++ src/MediaVC.Tools/TextDetector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MediaVC.Tools
+{
+    public static class TextDetector
+    {
+        public static bool CheckIsText(byte[] input)
+        {
+
+        }
+
+        public static bool CheckIsText(Memory<byte> input)
+        {
+
+        }
+
+        public static async ValueTask<bool> CheckIsTextAsync(Stream input)
+        {
+
+        }
+    }
+}
