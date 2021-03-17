5abc095817341f1ddccbb31291560bb149031054
Mateusz Okrój <mateuszokroj1@hotmail.com>
2021-03-17 17:05:28 +0100

    Added required classes for DifferenceCalculator

---
+++ src/MediaVC.Tools/Difference/DifferenceCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MediaVC.Difference;
+using MediaVC.Difference.FileSegments;
+
+namespace MediaVC.Tools.Difference
+{
+    public class DifferenceCalculator : IDifferenceCalculator
+    {
+        public IInputSource CurrentVersion { get; }
+        public IInputSource NewVersion { get; }
+        public IList<IFileSegmentInfo> Result { get; }
+
+        
+
+        public ValueTask CalculateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MediaVC.Tools/Difference/IDifferenceCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MediaVC.Difference;
+using MediaVC.Difference.FileSegments;
+
+namespace MediaVC.Tools.Difference
+{
+    public interface IDifferenceCalculator
+    {
+        ValueTask CalculateAsync();
+
+        IInputSource CurrentVersion { get; }
+
+        IInputSource NewVersion { get; }
+
+        IList<IFileSegmentInfo> Result { get; }
+    }
+}
