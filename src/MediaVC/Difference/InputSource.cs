ce31f838c8dc4e2c1cf8322a9c88295fe925d380
Mateusz Okrój <mateuszokroj1@hotmail.com>
2021-03-17 17:05:28 +0100

    Added required classes for DifferenceCalculator

---
+++ src/MediaVC/Difference/InputSource.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using MediaVC.Difference.FileSegments;
+using MediaVC.Difference.Strategies;
+
+namespace MediaVC.Difference
+{
+    public sealed class InputSource : Stream, IInputSource
+    {
+        public InputSource(FileStream file)
+        {
+
+        }
+
+        public InputSource(IEnumerable<IFileSegmentInfo> segments)
+        {
+
+        }
+
+        private readonly IInputSourceStrategy strategy;
+        public override bool CanRead { get; }
+        public override bool CanSeek { get; }
+        public override bool CanWrite { get; }
+        public override long Length { get; }
+        public override long Position { get; set; }
+
+        public override void Flush()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
