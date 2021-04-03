783ca2e97b048f1f8bccda9cfe8cc9eb38fc0efb
Mateusz Okrój <mateuszokroj1@gmail.com>
2021-04-03 17:13:36 +0200

    Added methods CheckSingleCharacter and CheckIsText(Stream). Added documentation.

--- src/MediaVC.Tools/TextDetector.cs
+++ src/MediaVC.Tools/TextDetector.cs
@@ -4,19 +4,42 @@ using System.Threading.Tasks;
 
 namespace MediaVC.Tools
 {
+    /// <summary>
+    /// Checks, that selected file is text-only
+    /// </summary>
     public static class TextDetector
     {
+        /// <summary>
+        /// Checks, that selected bytes is text-only
+        /// </summary>
+        /// <param name="input">Array to be checked</param>
         public static bool CheckIsText(byte[] input)
         {
 
         }
 
+        /// <summary>
+        /// Checks, that selected bytes is text-only
+        /// </summary>
+        /// <param name="input">Memory block to be checked</param>
         public static bool CheckIsText(Memory<byte> input)
+        {
+            
+        }
+
+        /// <summary>
+        /// Checks, that selected stream is text-only
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool CheckIsText(Stream input) => CheckIsTextAsync(input).Result;
+
+        public static async ValueTask<bool> CheckIsTextAsync(Stream input)
         {
 
         }
 
-        public static async ValueTask<bool> CheckIsTextAsync(Stream input)
+        private static bool CheckSingleCharacter(byte character)
         {
 
         }
