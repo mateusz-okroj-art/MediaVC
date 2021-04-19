a0681705036810830e3bc76c5b3cc8a70477cf86
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-04-19 23:40:15 +0200

    Created tests of constructors and properties for DifferenceCalculator.

---
+++ src/Tests/MediaVC.Tools.Tests/Difference/DifferenceCalculator/Properties.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediaVC.Difference.FileSegments;
+using MediaVC.Tools.Tests.Fixtures;
+
+using Xunit;
+
+namespace MediaVC.Tools.Tests.Difference.DifferenceCalculator
+{
+    public class Properties : IClassFixture<DifferenceCalculatorTestFixture>
+    {
+        #region Fields
+
+        private readonly DifferenceCalculatorTestFixture fixture;
+
+        #endregion
+
+        #region Constructor
+
+        public Properties(DifferenceCalculatorTestFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void Result_ShouldGetList()
+        {
+            Assert.IsType<List<IFileSegmentInfo>>(fixture.Calculator.Result);
+        }
+
+        [Fact]
+        public void SynchronizationContext_ShouldGetAndSet()
+        {
+            fixture.Calculator.SynchronizationContext = SynchronizationContext.Current;
+            Assert.Equal(SynchronizationContext.Current, fixture.Calculator.SynchronizationContext);
+        }
+
+        #endregion
+    }
+}
