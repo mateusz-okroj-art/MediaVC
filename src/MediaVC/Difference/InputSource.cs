8d8cfabed24b64e5ef3e298d9ebb7cf3dd662f8a
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-04-20 22:16:15 +0200

    Fixed build.

--- src/MediaVC/Difference/InputSource.cs
+++ src/MediaVC/Difference/InputSource.cs
@@ -10,15 +10,21 @@ namespace MediaVC.Difference
 {
     public sealed class InputSource : Stream, IInputSource, IEquatable<InputSource>
     {
+        #region Constructors
+
         public InputSource(FileStream file)
         {
-
+            throw new NotImplementedException();
         }
 
         public InputSource(IEnumerable<IFileSegmentInfo> segments)
         {
-
+            throw new NotImplementedException();
         }
+
+        #endregion
+
+        #region Properties
 
         internal IInputSourceStrategy Strategy { get; }
 
@@ -36,17 +42,20 @@ namespace MediaVC.Difference
             set => Strategy.Position = value;
         }
 
+        #endregion
+
+        #region Methods
 
         public override void Flush() => throw new InvalidOperationException();
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-
+            throw new NotImplementedException();
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-
+            throw new NotImplementedException();
         }
 
         public override void SetLength(long value) => throw new InvalidOperationException();
@@ -55,14 +64,16 @@ namespace MediaVC.Difference
 
         byte IInputSource.ReadByte()
         {
-            
+            throw new NotImplementedException();
         }
 
         public Memory<byte> ReadBytes(long count)
         {
-            
+            throw new NotImplementedException();
         }
 
         public bool Equals(InputSource other) => Strategy.Equals(other.Strategy);
+
+        #endregion
     }
 }
