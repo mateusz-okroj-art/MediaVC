def8557867f6d113d45fea8528edec355346eae4
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-04-28 00:09:57 +0200

    Modified input sources.

---
+++ src/MediaVC/Difference/Strategies/IFileSegmentMappingInfo.cs
@@ -0,0 +1,10 @@
+﻿using MediaVC.Difference.FileSegments;
+
+namespace MediaVC.Difference.Strategies
+{
+    internal interface IFileSegmentMappingInfo
+    {
+        IFileSegmentInfo Segment { get; set; }
+        long StartIndex { get; set; }
+    }
+}
