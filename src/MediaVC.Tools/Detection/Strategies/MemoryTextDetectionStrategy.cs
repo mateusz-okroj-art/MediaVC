d137927ca0a75f6b7f24613345261938a7fc1238
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-05-10 00:32:28 +0200

    Updated algorythm.

--- src/MediaVC.Tools/Detection/Strategies/MemoryTextDetectionStrategy.cs
+++ src/MediaVC.Tools/Detection/Strategies/MemoryTextDetectionStrategy.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,9 +26,47 @@ namespace MediaVC.Tools.Detection.Strategies
 
         #endregion
 
+        #region Methods
+
         public async ValueTask<bool> CheckIsTextAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var input = MemoryMarshal.ToEnumerable<byte>(Memory);
+
+            var length = input?.Count();
+
+            if(length < 1)
+                return false;
+            else if(length < 10_000)
+                foreach(var value in input)
+                {
+                    if(!CheckSingleCharacter(value))
+                        return false;
+                }
+            else
+            {
+                var stopped = false;
+                var locker = new object();
+
+                Parallel.ForEach(input, (value, state) =>
+                {
+                    if(!CheckSingleCharacter(value))
+                    {
+                        lock(locker)
+                            stopped = true;
+
+                        state.Break();
+                    }
+                });
+
+                return stopped;
+            }
+
+            return true;
         }
+
+        private static bool CheckSingleCharacter(byte character) =>
+            character is (0 or >= 8) and (<= 13 or >= 26);
+
+        #endregion
     }
 }
--- src/MediaVC.Tools/Detection/TextDetector.cs
+++ src/MediaVC.Tools/Detection/TextDetector.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -40,82 +39,6 @@ namespace MediaVC.Tools.Detection
         /// <param name="input">Enumerable object to be checked</param>
         public static bool CheckIsText(IEnumerable<byte> input)
         {
-            var length = input?.Count();
-
-            if (length < 1)
-                return false;
-            else if (length < 10_000)
-                foreach (var value in input)
-                {
-                    if (!CheckSingleCharacter(value))
-                        return false;
-                }
-            else
-            {
-                var stopped = false;
-                var locker = new object();
-
-                Parallel.ForEach(input, (value, state) =>
-                {
-                    if(!CheckSingleCharacter(value))
-                    {
-                        lock (locker) stopped = true;
-
-                        state.Break();
-                    }
-                });
-
-                return stopped;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Checks, that selected stream is text-only
-        /// </summary>
-        /// <exception cref="ArgumentNullException" />
-        /// <exception cref="IOException" />
-        /// <exception cref="OperationCanceledException" />
-        public static async ValueTask<bool> CheckIsTextAsync(Stream input, CancellationToken cancellationToken = default)
-        {
-            if (input == null)
-                throw new ArgumentNullException(nameof(input));
-
-            if (!input.CanRead)
-                throw new IOException("Cannot read.");
-
-            if (input.Length < 1)
-                return false;
-            else
-            {
-                try
-                {
-                    input.Position = 0;
-                    int value = -1;
-
-                    while((value = input.ReadByte()) != -1)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-
-                        if (!CheckSingleCharacter((byte)value))
-                            return false;
-                    }
-                }
-                catch(IOException)
-                {
-                    return false;
-                }
-
-                return true;
-            }
-        }
-
-        /// <summary>
-        /// Checks, that byte is text-based
-        /// </summary>
-        /// <param name="character">Byte to check</param>
-        private static bool CheckSingleCharacter(byte character) =>
-            character is (0 or >= 8) and (<= 13 or >= 26);
+            
     }
 }
