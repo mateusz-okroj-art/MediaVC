07b3bb814ed359d51745c09732fbfcb6508c8119
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-05-17 23:37:04 +0200

    Added tests for constructor.

--- src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/Constructors.cs
+++ src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/Constructors.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 
 using Xunit;
 using Moq;
+using System.IO;
+using MediaVC.Tools.Detection;
 
 namespace MediaVC.Tools.Tests.Detection.TextDetector
 {
     public class Constructors
     {
+        [Fact]
+        public void Constructor1_WhenArgumentIsNull_ShouldThrowException()
+        {
+            Stream stream = null;
 
+            Assert.Throws<ArgumentNullException>(() => new Tools.Detection.TextDetector(stream));
+        }
+
+        [Fact]
+        public void Constructor1_WhenArgumentIsValid_ShouldInitialize()
+        {
+            new Tools.Detection.TextDetector(Stream.Null);
+        }
+
+        [Fact]
+        public void Constructor2_WhenArgumentIsEmpty_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Tools.Detection.TextDetector(Memory<byte>.Empty));
+        }
+
+        [Fact]
+        public void Constructor2_WhenArgumentIsValid_ShouldInitialize()
+        {
+            new Tools.Detection.TextDetector(new Memory<byte>(new byte[1]));
+        }
     }
 }
