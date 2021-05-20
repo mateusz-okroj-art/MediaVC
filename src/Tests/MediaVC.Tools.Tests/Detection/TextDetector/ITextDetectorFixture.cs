4535f69dce8439d2167f15a9d2a6dbe081803207
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-05-20 19:11:37 +0200

    New tests for detector.

--- src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/ITextDetectorFixture.cs
+++ src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/ITextDetectorFixture.cs
@@ -1,6 +1,18 @@
-﻿namespace MediaVC.Tools.Tests.Detection.TextDetector
+﻿using System;
+using System.IO;
+
+namespace MediaVC.Tools.Tests.Detection.TextDetector
 {
-    public interface ITextDetectorFixture
+    public interface ITextDetectorFixture : IDisposable
     {
+        Memory<byte> Memory_Text1 { get; }
+        Memory<byte> Memory_Text2 { get; }
+        Memory<byte> Memory_NonText1 { get; }
+        Memory<byte> Memory_NonText2 { get; }
+
+        Stream Stream_Text1 { get; }
+        Stream Stream_Text2 { get; }
+        Stream Stream_NonText1 { get; }
+        Stream Stream_NonText2 { get; }
     }
 }
--- src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/TextDetectorFixture.cs
+++ src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/TextDetectorFixture.cs
@@ -9,8 +9,54 @@ namespace MediaVC.Tools.Tests.Detection.TextDetector
 {
     public sealed class TextDetectorFixture : ITextDetectorFixture
     {
+        #region Constructor
+
+        public TextDetectorFixture()
+        {
+            var dataText = new byte[100_000];
+            _ = Parallel.For(0, 100_000, index => dataText[index] = (byte)(index % 128));
+
+            Memory_Text2 = new Memory<byte>(dataText);
+            Stream_Text2 = new MemoryStream(dataText);
+
+            var dataNonText = new byte[100_001];
+
+            Memory_NonText2 = new Memory<byte>(dataNonText);
+            Stream_NonText2 = new MemoryStream(dataNonText);
+        }
+
+        #endregion
+
+        #region Properties
+
         public Memory<byte> Memory_Text1 { get; } = new Memory<byte>(new byte[1]);
 
+        public Memory<byte> Memory_Text2 { get; }
 
+        public Memory<byte> Memory_NonText1 { get; } = new Memory<byte>(new byte[]{ 255 });
+
+        public Memory<byte> Memory_NonText2 { get; }
+
+        public Stream Stream_Text1 { get; } = new MemoryStream(new byte[1]);
+
+        public Stream Stream_Text2 { get; }
+
+        public Stream Stream_NonText1 { get; } = new MemoryStream(new byte[] { 128 });
+
+        public Stream Stream_NonText2 { get; }
+
+        #endregion
+
+        #region Methods
+
+        public void Dispose()
+        {
+            Stream_Text1.Close();
+            Stream_Text2.Close();
+            Stream_NonText1.Close();
+            Stream_NonText2.Close();
+        }
+
+        #endregion
     }
 }
