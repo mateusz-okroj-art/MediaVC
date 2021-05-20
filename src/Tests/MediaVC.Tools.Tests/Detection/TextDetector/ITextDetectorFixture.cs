fce0fe60069b884037d96ea48293ceae6a6431c6
Mateusz Okrój <mateuszokroj1@gmail.com>
2021-05-20 22:52:44 +0200

    [US 283] Text stream detector (#1)
    [US 283] Text stream detector

---
+++ src/Tests/MediaVC.Tools.Tests/Detection/TextDetector/ITextDetectorFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace MediaVC.Tools.Tests.Detection.TextDetector
+{
+    public interface ITextDetectorFixture : IDisposable
+    {
+        Memory<byte> Memory_Text1 { get; }
+        Memory<byte> Memory_Text2 { get; }
+        Memory<byte> Memory_NonText1 { get; }
+        Memory<byte> Memory_NonText2 { get; }
+
+        Stream Stream_Text1 { get; }
+        Stream Stream_Text2 { get; }
+        Stream Stream_NonText1 { get; }
+        Stream Stream_NonText2 { get; }
+    }
+}
