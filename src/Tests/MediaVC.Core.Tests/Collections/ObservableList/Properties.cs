325a6f56111a09581017ebb8897f1af2a60becd4
Mateusz Okrój <mateuszokroj1@gmail.com>
2021-11-09 14:48:21 +0100

    Completed tests for ObservableList.

---
+++ src/Tests/MediaVC.Core.Tests/Collections/ObservableList/Properties.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace MediaVC.Core.Tests.Collections.ObservableList
+{
+    public class Properties
+    {
+        [Fact]
+        public void Locker_ShouldReadWrite()
+        {
+            var result = new ObservableList<object>();
+
+            Assert.IsType<object>(result.Locker);
+
+            var newValue = new object();
+            result.Locker = newValue;
+
+            Assert.Equal(newValue, result.Locker);
+        }
+
+        [Fact]
+        public void IsReadonly_ShouldReturnFalse()
+        {
+            var result = new ObservableList<object>();
+
+            Assert.False(result.IsReadOnly);
+        }
+    }
+}
