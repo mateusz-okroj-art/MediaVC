c7ec1eb8a3d746716d45604ec9e5cb1f45a9c58e
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-11-12 01:04:12 +0100

    Created benchmark tests.

---
+++ src/Tests/MediaVC.Tools.Benchmark/ConsoleProgressReporter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MediaVC.Tools.Benchmark
+{
+    internal class ConsoleProgressReporter : IProgress<float>
+    {
+        public void Report(float value) => Console.WriteLine($"{value * 100}%");
+    }
+}
---
+++ src/Tests/MediaVC.Tools.Benchmark/DifferenceCalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediaVC.Difference;
+
+using static System.Console;
+
+namespace MediaVC.Tools.Benchmark
+{
+    internal static class DifferenceCalculator
+    {
+        public static void Run()
+        {
+            var files = Array.Empty<FileStream>();
+
+            try
+            {
+                WriteLine("1) Difference scanning of large files");
+                WriteLine("   Generating large files...");
+                files = GenerateFiles();
+
+                WriteLine("Test with two files.");
+
+            }
+            finally
+            {
+                if(files is not null)
+                {
+                    foreach(var file in files)
+                        file?.Dispose();
+                }
+            }
+        }
+
+        private static FileStream[] GenerateFiles()
+        {
+            var files = new FileStream[2];
+            var tempDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "temp");
+
+            for(byte count = 0; count < 2; ++count)
+            {
+                var filename = Path.Combine(tempDirectory, $"{Guid.NewGuid()}.tmp");
+
+                var file = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+                file.SetLength(2_000_000_000);
+
+                files[count] = file;
+            }
+
+            return files;
+        }
+
+        private static void Calc1(FileStream file1, FileStream file2)
+        {
+            var currentVersionSource = new InputSource(file1);
+            var newVersionSource = new InputSource(file2);
+
+            var progressReporter = new ConsoleProgressReporter();
+            var calculator = new Difference.DifferenceCalculator(currentVersionSource, newVersionSource);
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            calculator.CalculateAsync(CancellationToken.None, progressReporter).AsTask().Wait();
+
+            stopwatch.Stop();
+
+            WriteLine($"Time: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
