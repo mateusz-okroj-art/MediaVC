478b9dfecd5dbf7e8ec782b27c5b40ec25809f9d
Mateusz Okrój <mateuszokroj1@hotmail.com>
2021-12-10 00:30:02 +0100

    Added enumerator tests.

--- src/Tests/MediaVC.Core.Tests/Difference/InputSource/Methods.cs
+++ src/Tests/MediaVC.Core.Tests/Difference/InputSource/Methods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -49,6 +50,28 @@ namespace MediaVC.Core.Tests.Difference.InputSource
             await result.ReadByteAsync(CancellationToken.None);
 
             mock.Verify(mocked => mocked.ReadByteAsync(It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async void Enumerator_ShouldEnumerateBytesFromStream()
+        {
+            var strategyMock = new Mock<IInputSourceStrategy>();
+            strategyMock.SetupProperty(mock => mock.Position);
+            strategyMock.SetupGet(mock => mock.Length).Returns(10);
+            strategyMock.Setup(mock => mock.ReadByteAsync(It.IsAny<CancellationToken>()))
+                .Returns(ValueTask.FromResult<byte>(0));
+
+            using var inputSource = new MediaVC.Difference.InputSource(strategyMock.Object);
+
+            var results = new List<byte>(10);
+            await foreach(var item in inputSource)
+                results.Add(item);
+
+            Assert.Equal(strategyMock.Object.Length, results.Count);
+            Assert.All(results, item => Assert.Equal(0, item));
+
+            strategyMock.Verify(mock => mock.ReadByteAsync(It.IsAny<CancellationToken>()), Times.Exactly(10));
+            strategyMock.VerifySet(mock => mock.Position = It.IsInRange<long>(-1, 9, Moq.Range.Inclusive));
         }
     }
 }
