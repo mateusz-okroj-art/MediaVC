f0e1d169e861c996d565005d05640d8a96682190
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-01-01 18:04:42 +0100

    First implementation of reading engine.

--- src/MediaVC.Core/Helpers/TextReadingEngine.cs
+++ src/MediaVC.Core/Helpers/TextReadingEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,6 +16,36 @@ namespace MediaVC.Helpers
 
         public async ValueTask<char?> ReadAsync(CancellationToken cancellationToken = default)
         {
+            if(source.Position >= source.Length)
+                return null;
+
+            if(source.Position == source.Length - 1)
+            {
+                var byte1 = await source.ReadByteAsync(cancellationToken);
+
+                return byte1 > 127
+                    ? throw new InvalidOperationException("Byte is not segment of char.")
+                    : Encoding.UTF8.GetChars(new byte[] { byte1 })[0];
+            }
+            else
+            {
+                var byte1 = await source.ReadByteAsync(cancellationToken);
+
+                if(byte1 >> 5 == 0b111) // UTF-8 - first segment of three part char
+                {
+
+                }
+                else if(byte1 >> 6 == 0b11) // UTF-8 - first segment of two part char
+                {
+
+                }
+                else
+                {
+                    return byte1 > 127
+                    ? throw new InvalidOperationException("Byte is not segment of char.")
+                    : Encoding.UTF8.GetChars(new byte[] { byte1 })[0];
+                }
+            }
 
         }
     }
