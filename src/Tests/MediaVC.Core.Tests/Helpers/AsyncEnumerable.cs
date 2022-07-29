70ae01b9bd2b7a31400006f2b08d7e154f2b76d2
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-07-30 00:50:14 +0200

    Added tests for AsyncEnumerable.

---
+++ src/Tests/MediaVC.Core.Tests/Helpers/AsyncEnumerable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using Moq;
+
+using Xunit;
+
+namespace MediaVC.Core.Tests.Helpers
+{
+    public class AsyncEnumerable
+    {
+        [Fact]
+        public void Constructor_WhenArgumentIsNotNull_ShouldReturnValid()
+        {
+            var mocked = Mock.Of<IAsyncEnumerator<object>>();
+            var cancel = new CancellationTokenSource();
+
+            var result = new AsyncEnumerable<object>(mocked);
+
+            Assert.Equal(mocked, result.GetAsyncEnumerator());
+            Assert.IsAssignableFrom<IAsyncEnumerator<object>>(result.GetAsyncEnumerator(cancel.Token));
+        }
+
+        [Fact]
+        public void Constructor_WhenArgumentIsNull_ShouldThrow()
+        {
+            IAsyncEnumerator<object> argument = null;
+
+            Assert.Throws<ArgumentNullException>(() => new AsyncEnumerable<object>(argument));
+        }
+    }
+}
