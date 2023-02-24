59456e80a952d183a24f3356a36030555745287f
Mateusz Okrój <mateuszokroj1@gmail.com>
2023-02-24 17:05:32 +0100

    Created new interface for repository access.

---
+++ src/MediaVC/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace MediaVC
+{
+
+    /// <summary>
+    /// Provides access to operate on single MediaVC repository
+    /// </summary>
+    public interface IRepository
+    {
+        ValueTask AddChangeset(object changeset);
+
+        ValueTask DownloadAsync();
+
+        ValueTask UploadAsync();
+    }
+}
---
+++ src/MediaVC/Repository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MediaVC
+{
+    public sealed class Repository : IRepository
+    {
+        #region Constructors
+
+        public Repository(IRepositorySource source)
+        {
+            this.source = source ?? throw new ArgumentNullException(nameof(source));
+        }
+
+        internal Repository() { }
+
+        #endregion
+
+        #region Fields
+
+        private IRepositorySource? source;
+
+        #endregion
+
+        #region Methods
+
+        public ValueTask DownloadAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask UploadAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask AddChangeset(object changeset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Repository Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
